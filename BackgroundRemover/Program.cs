8a49aca3bbb7557a86076a2c73e16c4e75c54114
Will <willarich@gmail.com>
2022-07-05 13:37:43 -0700

    End of section 4

--- BackgroundRemover/Program.cs
+++ BackgroundRemover/Program.cs
@@ -5,7 +5,10 @@ using System.Drawing.Imaging;
 
 Console.WriteLine("Hello, World!");
 
-string[] files = Directory.GetFiles("./Images");
+var path = "./Images";
+//var path = "./";
+
+string[] files = Directory.GetFiles(path);
 
 foreach (var file in files)
 {
@@ -16,7 +19,10 @@ foreach (var file in files)
     var test1 = bitmap.GetPixel(0,0);
     Console.WriteLine(test1.Name);
     Console.WriteLine(test1.ToString());
-    Console.WriteLine("No, " + test1.Equals(Color.White) + test1.Equals(Color.FromArgb(247,247,247)));
+    Console.WriteLine("No, " + test1.Equals(Color.FromArgb(255,255,255)) + test1.Equals(Color.FromArgb(247,247,247)));
+    if (!test1.Equals(Color.FromArgb(255,255,255)) 
+    && !test1.Equals(Color.FromArgb(247,247,247)))
+        continue;
 
     
     for(var x=0; x<bitmap.Width; x++)
@@ -25,7 +31,7 @@ foreach (var file in files)
         {
             Color pixelColor = bitmap.GetPixel(x, y);
             Color newColor = Color.FromArgb(0, pixelColor);//Color.FromArgb(pixelColor.R, 0, 0);
-            if (pixelColor.Equals(Color.White) || pixelColor.Equals(Color.FromArgb(247,247,247)))
+            if (pixelColor.Equals(Color.FromArgb(255,255,255)) || pixelColor.Equals(Color.FromArgb(247,247,247)))
             {
                 bitmap.SetPixel(x, y, newColor);
             }
@@ -35,7 +41,12 @@ foreach (var file in files)
     }
 
     Directory.CreateDirectory("./Output");
-    bitmap.Save($"./Output/test{new Random().Next(100)}.png", ImageFormat.Png);
+    //bitmap.Save($"./Output/test{new Random().Next(100)}.png", ImageFormat.Png);
+    var newFileName = file.Replace(path, "");
+    newFileName = newFileName.Replace(@"\", "");
+
+    Console.WriteLine("NEW " + newFileName);
+    bitmap.Save(newFileName, ImageFormat.Png);
 }
 
 ;
